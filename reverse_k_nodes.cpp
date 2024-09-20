863e9387ff1052b42a59320e0ab5a4d644d5a27b
Prannessh <99811495+Prannessh2006@users.noreply.github.com>
2024-09-21 00:10:06 +0530

    Create reverse_k_nodes.cpp

---
+++ reverse_k_nodes.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node(int value): data(value){
+        next=nullptr;
+    }
+};
+Node* insertatendintorder(Node* head,int value){
+    Node* newnode = new Node(value);
+    if(head==nullptr){
+        head=newnode;
+        return head;
+    }
+    else{
+        if(head->data>=value){
+            newnode->next=head;
+            head=newnode;
+            return head;
+        }
+        else{
+            Node* curr=head;
+            while(curr->next!=NULL){
+                if(curr->next->data>value){
+                    newnode->next=curr->next;
+                    curr->next=newnode;
+                    return head;
+                }
+                curr=curr->next;
+            }
+            curr->next=newnode;
+            return head;
+        }
+    }
+}
+
+
+Node* knodesreverse(Node* head,int k){
+    Node* curr=head;
+    Node* prev=nullptr;
+    Node* next=nullptr;
+    int count=0;
+    while(curr!=NULL && count<k){
+        next = curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count++;
+    }
+    Node* again=prev;
+    while(again->next!=nullptr){
+        again=again->next;
+    }
+    again->next=curr;
+    return prev;
+}
+
+
+void printlist(Node* head){
+    Node* curr=head;
+    while(curr!=NULL){
+        cout << curr->data <<" ";
+        curr=curr->next;
+    }
+}
+int main(){
+    int size1,k;
+    cin >> size1;
+    int value;
+    Node* head1=nullptr;
+    for(int i=0;i<size1;i++){
+        cin >> value;
+        head1 = insertatendintorder(head1,value);
+    }
+    cin >> k;
+    head1 = knodesreverse(head1,k);
+    printlist(head1);
+    
+    
+}
