46a731f775a1c56827ddfd29c2a4f3ad5bd17cbc
Prannessh <99811495+Prannessh2006@users.noreply.github.com>
2024-11-27 23:30:58 +0530

    Create infixtoprefix.cpp

---
+++ infixtoprefix.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isoperator(char a){
+    if(a=='*' or a=='/' or a=='+' or a=='-'){
+        return true;
+    }
+    return false;
+}
+bool higherprecedence(char a,char b){
+    if((a=='*' or a=='/') and (b=='+' or b=='-')){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+string itp(string str){
+    stack<char> st;
+    string temp="";
+    for(int i=0;i<str.length();i++){
+        if(isoperator(str[i])){
+            if(!st.empty() && higherprecedence(st.top(),str[i])){
+                while(!st.empty()){
+                    temp+=st.top();
+                    st.pop();
+                }
+            }
+            st.push(str[i]);
+            }
+        else{
+            temp+=str[i];
+        }
+    }
+    while(!st.empty()){
+        temp+=st.top();
+        st.pop();
+    }
+    return temp;
+}
+int main(){
+    string str="A*B+C/D";
+    cout<<"Infix: "<<str<<endl;
+    str=itp(str);
+    cout<<"postfix: "<<str;
+}
