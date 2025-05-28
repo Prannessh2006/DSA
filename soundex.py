951c4255618bb76445375469625d62562552e864
Prannessh <99811495+Prannessh2006@users.noreply.github.com>
2025-05-28 21:15:14 +0530

    soundex.py

---
+++ soundex.py
@@ -0,0 +1,35 @@
+x = str(input("Enter a name: "))
+x=x.lower()
+lst_0 = ['a','i','o','u','e','h','w','y']
+lst_1 = ['b','f','p','v']
+lst_2 = ['c','g','j','k','s','x','z']
+lst_3 = ['d','t']
+lst_4 = ['l']
+lst_5 = ['m','n']
+lst_6 = ['r'] 
+x_1=[] 
+for i in range(len(x)):
+  if(i==0):
+    x_1.append(x[i].upper())
+  elif(x[i]==x[i-1]):
+    continue
+  elif(x[i] in lst_0):
+    continue
+  elif(x[i] in lst_1):
+    x_1.append('1')
+  elif(x[i] in lst_2):
+    x_1.append('2')
+  elif(x[i] in lst_3):
+    x_1.append('3')
+  elif(x[i] in lst_4):
+    x_1.append('4')
+  elif(x[i] in lst_5):
+    x_1.append('5')
+  elif(x[i] in lst_6):
+    x_1.append('6')
+  else:
+    continue
+while(len(x_1)<4):
+  x_1.append('0')
+print("".join(x_1))
+  
