690aff65df9efc8be79194be6baae334b3a69851
Prannessh <99811495+Prannessh2006@users.noreply.github.com>
2025-06-05 01:09:51 +0530

    Update t_test.py

--- t_test.py
+++ t_test.py
@@ -4,4 +4,4 @@ def t_test(arr_1,arr_2):
   m_1,m_2 = np.mean(arr_1),np.mean(arr_2)
   var_1,var_2=np.var(arr_1,ddof=1),np.var(arr_2,ddof=1)
   s = (var_1+var_2)/(n1+n2-2)
-  return  (m_1-m_2)/np.sqrt(s*(1/n1+1/n2))
+  return  (m_1-m_2)/np.sqrt(s*(1/n_1+1/n_2))
