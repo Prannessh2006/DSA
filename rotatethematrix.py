5a9ec5842c24448b3fb28d3559445ae5002d5e37
Prannessh KVA <99811495+Prannessh2006@users.noreply.github.com>
2025-12-05 18:45:27 +0530

    Create rotatethematrix.py

---
+++ rotatethematrix.py
@@ -0,0 +1,7 @@
+matrix=[[.......]]
+for i in range(len(matrix)):
+  for j in range(i,len(matrix)):
+    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+for i in range(len(matrix)):
+  matrix[i].reverse()
+print(matrix)
